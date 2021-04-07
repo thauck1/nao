be691cf4c3bb933e0f11651e401d4e1d07f466cc
Foster Library <fosterlibrary@Devins-MacBook-Pro.local>
2021-04-07 15:29:04 +0900

    added other applications

---
+++ LearnFaces/translations/translation_en_US.ts
@@ -0,0 +1,204 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/LearnFace/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Great! I learned a new face!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Great! I learned a new face!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/LearnFace/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hmm- it didn't work. Do you want to try again?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hmm- it didn't work. Do you want to try again?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/LearnFace/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Let's learn a new face! Who's face am I learning?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Let's learn a new face! Who's face am I learning?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Great! I learned a new face!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Great! I learned a new face!</translation>
+        </message>
+        <message>
+            <source>That's Devin!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">That's Devin!</translation>
+        </message>
+        <message>
+            <location filename="../../../../../../../../../../../translations/behavior_1/behavior.xar" line="0"/>
+            <source></source>
+            <comment>Text</comment>
+            <translation></translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I know you!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I know you!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hmm- it didn't work. Do you want to try again?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hmm- it didn't work. Do you want to try again?</translation>
+        </message>
+        <message>
+            <source>Let's see how I am at recognizing friends. Should I recognize someone or learn a new face?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Let's see how I am at recognizing friends. Should I recognize someone or learn a new face?</translation>
+        </message>
+        <message>
+            <source>&quot;Let's see how I am at recognizing friends. Should I recognize someone or learn a new face?&quot;</source>
+            <comment>Text</comment>
+            <translation type="obsolete">&quot;Let's see how I am at recognizing friends. Should I recognize someone or learn a new face?&quot;</translation>
+        </message>
+        <message>
+            <source>&quot;Should I learn a new face or recognize someone?&quot;</source>
+            <comment>Text</comment>
+            <translation type="obsolete">&quot;Should I learn a new face or recognize someone?&quot;</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>&quot;Should I learn a new face or recognize someone&quot;</source>
+            <comment>Text</comment>
+            <translation type="unfinished">&quot;Should I learn a new face or recognize someone&quot;</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>That's Mister Hut-sell!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">That's Mister Hut-sell!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>Hello Kaoloo!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello Kaoloo!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (4)</name>
+        <message>
+            <source>Hi Elan!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hi Elan!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (5)</name>
+        <message>
+            <source>Hi Doctor Smilie!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hi Doctor Smilie!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (6)</name>
+        <message>
+            <source>Hello Mister Saldaya!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello Mister Saldaya!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (7)</name>
+        <message>
+            <source>Good Day Mister Geeseck!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good Day Mister Geeseck!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say Phrase to Recognized Face/Say</name>
+        <message>
+            <source>That's Devin!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">That's Devin!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say Phrase to Recognized Face/Say (2)</name>
+        <message>
+            <source>That's Mister Hut-sell!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">That's Mister Hut-sell!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say Phrase to Recognized Face/Say (3)</name>
+        <message>
+            <source>Hello Kaoloo!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello Kaoloo!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say Phrase to Recognized Face/Say (4)</name>
+        <message>
+            <source>Hi Elan!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hi Elan!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say Phrase to Recognized Face/Say (5)</name>
+        <message>
+            <source>Hi Doctor Smilie!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hi Doctor Smilie!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say Phrase to Recognized Face/Say (6)</name>
+        <message>
+            <source>Hello Mister Saldaya!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello Mister Saldaya!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say Phrase to Recognized Face/Say (7)</name>
+        <message>
+            <source>Good Day Mister Geeseck!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good Day Mister Geeseck!</translation>
+        </message>
+    </context>
+</TS>
---
+++ LibraryInfoGeneral/LibraryInfoGeneral.pml
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="LibraryInfoGeneral" format_version="5">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="LibraryGeneralInfo" src="LibraryGeneralInfo/LibraryGeneralInfo.dlg" />
+    </Dialogs>
+    <Resources />
+    <Topics>
+        <Topic name="LibraryGeneralInfo_enu" src="LibraryGeneralInfo/LibraryGeneralInfo_enu.top" topicName="LibraryGeneralInfo" language="en_US" nuance="enu" />
+        <Topic name="LibraryGeneralInfo_jpj" src="LibraryGeneralInfo/LibraryGeneralInfo_jpj.top" topicName="LibraryGeneralInfo" language="ja_JP" nuance="jpj" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+        <Translation name="translation_ja_JP" src="translations/translation_ja_JP.ts" language="ja_JP" />
+    </Translations>
+</Package>
